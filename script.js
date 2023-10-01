e44fafdf5e7ab6be328cb8ee8c92372fa7eef99f
michael <michaelyoussef396@gmail.com>
2023-10-01 14:03:51 +1100

    added javascript to react to filling the forms out

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you, ${name}! Your message has been received. We'll get back to you at ${email}.`);
+});
