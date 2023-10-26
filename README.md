6efaa17827d172261164f556296cb71f29850976
michael <michaelyoussef396@gmail.com>
2023-10-26 12:11:11 +1100

    added github and SSh and styled the website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My Personal Website
+
+Here is my github URL link: https://github.com/michaelyoussef396/my-personal-website and my repository SSH: git@github.com:michaelyoussef396/my-personal-website.git
--- script.js
+++ script.js
@@ -34,3 +34,13 @@ profileImage.addEventListener("mouseover", function () {
 profileImage.addEventListener("mouseout", function () {
     profileImage.src = originalImageSrc;
 });
+
+function copyToClipboard(url) {
+    const tempInput = document.createElement('input');
+    tempInput.value = url;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Link copied to clipboard: ' + url);
+}
