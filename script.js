ea0f1b071649b2cc6f93103d9e0b8f2492649cd7
michael <michaelyoussef396@gmail.com>
2023-12-10 23:43:13 +1100

    added new styling

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
 
-    alert(`Thank you, ${name}! Your message has been received. We'll get back to you at ${email}.`);
+/*i added this for the switch color feature*/
+document.addEventListener("DOMContentLoaded", function () {
+    const colorSwitchBtn = document.getElementById('colorSwitchBtn');
+    const body = document.body;
+
+    colorSwitchBtn.addEventListener('click', function () {
+        body.classList.toggle('blue');
+    });
 });
 
-const profileImage = document.getElementById("profile-image");
-const toggleButton = document.getElementById("toggle-button");
+/*i added this for the messages button feature*/
 
-const originalImageSrc = profileImage.src;
-const newImageSrc = "https://upload.wikimedia.org/wikipedia/commons/1/1d/Red_sea_coast%2C_Makadi_bay.jpg";
-
-toggleButton.addEventListener("click", function () {
-    if (profileImage.style.display === "none") {
-        profileImage.src = originalImageSrc;
-        profileImage.style.display = "block";
-    } else {
-        profileImage.src = newImageSrc;
-        profileImage.style.display = "block";
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const greetingBtn = document.getElementById('btn-greeting');
+    const greetingMessage = document.getElementById('greeting-message');
 });
 
-profileImage.addEventListener("mouseover", function () {
-    if (profileImage.src === originalImageSrc) {
-        profileImage.src = newImageSrc;
-    } else {
-        profileImage.src = originalImageSrc;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const greetBtn = document.getElementById('greetBtn');
+    const greetingMessage = document.getElementById('greetingMessage');
+
+    greetBtn.addEventListener('click', function () {
+        const userName = prompt('What is your name?');
+        if (userName) {
+            const message = `Hello ${userName}, nice to meet you and thrilled you're here!`;
+            greetingMessage.textContent = message;
+            greetingMessage.style.display = 'block';
+            greetBtn.style.display = 'none';
+            greetingMessage.style.background = 'aqua';
+            greetingMessage.style.margin = '2rem 0';
+            greetingMessage.style.padding = '1rem 2rem';
+            greetingMessage.style.fontSize = '1.6rem';
+            greetingMessage.style.width = 'auto';
+            greetingMessage.style.borderRadius = '1.3rem';
+            greetingMessage.style.color = '#000';
+            greetingMessage.style.fontWeight = '600';
+        }
+    });
 });
 
-profileImage.addEventListener("mouseout", function () {
-    profileImage.src = originalImageSrc;
-});
 
-function copyToClipboard(url) {
-    const tempInput = document.createElement('input');
-    tempInput.value = url;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempInput);
-    alert('Link copied to clipboard: ' + url);
-}
+// added for contact list
+
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contact-Form');
+
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Get form values
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const message = document.getElementById('message').value;
+
+        // Validate email using a simple regex pattern
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValidEmail = emailPattern.test(email);
+
+        // Validate phone number (simple validation for demonstration)
+        const isValidPhone = phone.length >= 8;
+
+        if (name && isValidEmail && isValidPhone && message) {
+            alert(`Thank you, ${name}! I'll get in contact with you soon.`);
+            contactForm.reset(); // Reset the form
+        }
+    });
+});
